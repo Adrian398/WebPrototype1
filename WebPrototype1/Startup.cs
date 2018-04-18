ac0cfa54358f0c651b43cd1b277f0f51445e25a9
Adrian <Adrian@ADRIANS-PC>
2018-04-18 17:37:14 +0200

    w

---
+++ WebPrototype1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPrototype1.Startup))]
+namespace WebPrototype1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
